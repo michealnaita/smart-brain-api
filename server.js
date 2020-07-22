cef27f9715ae7f091839a61f41b3076c1d02c044
liz <micheal naita>
2020-07-22 21:41:13 +0300

    database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image.js");
 const db = knex({
 	client: "pg",
 	connection: {
-		host: "127.0.0.1",
-		user: "liz pc",
-		password: "naita",
-		database: "smart-brain",
+		connectionString: process.env.DATABASE_URL,
+		ssl: true,
 	},
 });
 // db.select("*")
